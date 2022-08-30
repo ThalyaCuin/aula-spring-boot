14dcaf2bd27120be426c82fa2508349cc845226c
Thalya <thalyacuin@gmail.com>
2022-08-30 11:21:26 -0300

    first commit

---
+++ Grupo02/src/test/java/Grupo02/Grupo02ApplicationTests.java
@@ -0,0 +1,13 @@
+package Grupo02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
